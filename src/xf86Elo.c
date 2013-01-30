c666c78c764d0b97fa25bd9f0796a83b77761f7d
Peter Hutterer <peter.hutterer@who-t.net>
2013-01-30 11:47:03 +1000

    Handle DEVICE_ABORT on input ABI 19.1
    Don't do anything, but don't print a warning either.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/xf86Elo.c
+++ src/xf86Elo.c
@@ -875,6 +875,11 @@ xf86EloControl(DeviceIntPtr	dev,
     DBG(2, ErrorF("Done\n"));
     return Success;
 
+#if GET_ABI_MAJOR(ABI_XINPUT_VERSION) * 100 + GET_ABI_MINOR(ABI_XINPUT_VERSION) >= 1901
+  case DEVICE_ABORT:
+    return Success;
+#endif
+
   default:
       ErrorF("unsupported mode=%d\n", mode);
       return BadValue;
